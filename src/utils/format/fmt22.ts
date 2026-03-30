20fc6d6ff4e9a6f7397888889bb0cc874316c2e2
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:11:32 +0600

    refactor: add format utils set 22

---
+++ src/utils/format/fmt22.ts
@@ -0,0 +1,3 @@
+export const formatUSD22=(v:number)=>'$'+v.toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2});
+export const formatPct22=(v:number)=>(v>=0?'+':'')+v.toFixed(2)+'%';
+export const formatSTX22=(v:string)=>(parseInt(v)/1e6).toFixed(6)+' STX';
