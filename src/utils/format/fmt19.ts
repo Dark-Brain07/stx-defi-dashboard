1263eb14ab7189538cd1eb92814073378e1d1296
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:11:31 +0600

    refactor: add format utils set 19

---
+++ src/utils/format/fmt19.ts
@@ -0,0 +1,3 @@
+export const formatUSD19=(v:number)=>'$'+v.toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2});
+export const formatPct19=(v:number)=>(v>=0?'+':'')+v.toFixed(2)+'%';
+export const formatSTX19=(v:string)=>(parseInt(v)/1e6).toFixed(6)+' STX';
