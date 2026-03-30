81f9af449cab685692fd47933dacc82b8fa92e0d
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:11:28 +0600

    refactor: add format utils set 13

---
+++ src/utils/format/fmt13.ts
@@ -0,0 +1,3 @@
+export const formatUSD13=(v:number)=>'$'+v.toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2});
+export const formatPct13=(v:number)=>(v>=0?'+':'')+v.toFixed(2)+'%';
+export const formatSTX13=(v:string)=>(parseInt(v)/1e6).toFixed(6)+' STX';
