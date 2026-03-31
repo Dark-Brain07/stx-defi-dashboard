b2c5b5f1a7db52c15ba39ab964c8a76b9ba739b6
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-31 08:58:13 +0600

    refactor: add chain helper utils 2

---
+++ src/utils/chain/helper2.ts
@@ -0,0 +1,4 @@
+export const toMicroSTX2=(stx:number):bigint=>BigInt(Math.round(stx*1e6));
+export const fromMicroSTX2=(micro:string):number=>parseInt(micro)/1e6;
+export const shortenAddr2=(addr:string,chars=4):string=>addr.slice(0,chars+2)+'...'+addr.slice(-chars);
+export const isValidSTXAddr2=(addr:string):boolean=>/^S[PM][A-Z0-9]{38,40}$/.test(addr);
